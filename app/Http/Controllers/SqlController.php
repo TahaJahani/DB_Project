67f86442dc67e3c9713efe3bb0a467252a676670
TahaJahani <taha7900@gmail.com>
2022-02-08 00:08:19 +0330

    model field data updated

--- app/Http/Controllers/SqlController.php
+++ app/Http/Controllers/SqlController.php
@@ -29,23 +29,23 @@ class SqlController extends Controller
     public function getModelData()
     {
         return [
-            "Address" => ["national_id", "postal_code", "province", "city", "rest"],
-            "BankingTransaction" => ["transaction_id", "bank_name", "shaparak_code"],
-            "Bookmark" => ["national_id", "product_id"],
-            "Cart" => ["cart_id", "national_id"],
-            "CartProduct" => ["cart_id", "product_id", "quantity"],
-            "Category" => ["category_id", "name", "parent_category_id"],
-            "Comment" => ["national_id", "product_id", "has_bought", "score", "text", "date", "status"],
-            "ConsumableProduct" => ["product_id", "product_date", "expiration_date"],
-            "Customer" => ["national_id", "loyalty_score"],
-            "InPlaceTransaction" => ["transaction_id", "method"],
-            "Order" => ["order_id", "cart_id", "status", "date", "payment_method", "transaction_id", "postman_national_id", "address_id"],
-            "Person" => ["national_id", "email", "first_name", "last_name", "telephonr", "mobile"],
-            "Postman" => ["national_id", "salary"],
-            "Product" => ["product_id", "name", "description", "quantity", "sale_price", "purchase_price", "discount", "img_url", "category_id"],
-            "Property" => ["product_id", "key", "value"],
-            "Supplier" => ["national_id", "company_name", "average_score"],
-            "Transaction" => ["transaction_id", "national_id", "type"],
+            "Address" => ["table_name" => 'addresses', "fields" => ["national_id", "postal_code", "province", "city", "rest"]],
+            "BankingTransaction" => ["table_name" => "banking_transactions", "fields" => ["transaction_id", "bank_name", "shaparak_code"]],
+            "Bookmark" => ["table_name" => "bookmarks", "fields" => ["national_id", "product_id"]],
+            "Cart" => ["table_name" => "carts", "fields" => ["cart_id", "national_id"]],
+            "CartProduct" => ["table_name" => "cart_products", "fields" => ["cart_id", "product_id", "quantity"]],
+            "Category" => ["table_name" => "categories", "fields" => ["category_id", "name", "parent_category_id"]],
+            "Comment" => ["table_name" => "comments", "fields" => ["national_id", "product_id", "has_bought", "score", "text", "date", "status"]],
+            "ConsumableProduct" => ["table_name" => "consumable_products", "fields" => ["product_id", "product_date", "expiration_date"]],
+            "Customer" => ["table_name" => "customers", "fields" => ["national_id", "loyalty_score"]],
+            "InPlaceTransaction" => ["table_name" => "in_place_transactions", "fields" => ["transaction_id", "method"]],
+            "Order" => ["table_name" => "orders", "fields" => ["order_id", "cart_id", "status", "date", "payment_method", "transaction_id", "postman_national_id", "address_id"]],
+            "Person" => ["table_name" => "people", "fields" => ["national_id", "email", "first_name", "last_name", "telephonr", "mobile"]],
+            "Postman" => ["table_name" => "postmen", "fields" => ["national_id", "salary"]],
+            "Product" => ["table_name" => "products", "fields" => ["product_id", "name", "description", "quantity", "sale_price", "purchase_price", "discount", "img_url", "category_id"]],
+            "Property" => ["table_name" => "properties", "fields" => ["product_id", "key", "value"]],
+            "Supplier" => ["table_name" => "suppliers", "fields" => ["national_id", "company_name", "average_score"]],
+            "Transaction" => ["table_name" => "transactions", "fields" => ["transaction_id", "national_id", "type"]],
         ];
     }
 
@@ -91,7 +91,7 @@ class SqlController extends Controller
                     $query->$name($param[0]);
             }
         }
-        if ($query_name == 'select'){
+        if ($query_name == 'select') {
             $args = $request->query_params;
             $data = $query->addSelect($args)->get();
         } else if ($query_name == 'update') {
